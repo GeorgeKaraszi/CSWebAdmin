d843bfd68ec6bdf0c467dc3fa545a9bb262b51c5
George Karaszi <GeorgeKaraszi@gmail.com>
2016-04-30 11:59:12 -0400

    Added some basic counting information to the main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+  before_action :set_counters, only: [:index]
   def index
+  end
+
+  private
+  def set_counters
+    @group_counter = LdapGroup.all.count
+    @user_counter = LdapUser.all.count
   end
 end
